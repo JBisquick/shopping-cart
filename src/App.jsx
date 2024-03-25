564ac5c85187701107fdab086f4c7f5bcbd8e881
JBisquick <felipebirch@gmail.com>
2024-03-25 16:08:43 -0700

    Add routing between home, shopping, and cart page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { useParams } from 'react-router-dom';
 import Navigation from './components/Navigation';
+import Home from './components/Home';
+import Cart from './components/Cart';
+import Shopping from './components/Shopping';
 
 function App() {
+  const { name } = useParams();
+
   return (
     <div>
       <Navigation></Navigation>
+      {name === 'shopping' ? (
+        <Shopping></Shopping>
+      ) : name === 'cart' ? (
+        <Cart></Cart>
+      ) : (
+        <Home></Home>
+      )}
     </div>
   );
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,13 @@
+import { Link } from 'react-router-dom';
+
 function Navigation() {
   return (
     <div>
       <h1>Odin Shopping</h1>
       <div>
-        <h3>Home</h3>
-        <h3>Shopping</h3>
-        <h3>Cart</h3>
+        <Link to="/">Home</Link>
+        <Link to="/shopping">Shopping</Link>
+        <Link to="/cart">Cart</Link>
       </div>
     </div>
   );
