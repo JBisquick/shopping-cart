5d2791353608af05bf71a48683dbc33c634d7421
JBisquick <felipebirch@gmail.com>
2024-03-26 12:49:19 -0700

    Add fetching and rendering for shopping items

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import Navigation from './components/Navigation';
 import Home from './components/Home';
 import Cart from './components/Cart';
 import Shopping from './components/Shopping';
+import useItems from './hooks/useItems';
 
 function App() {
   const { name } = useParams();
+  const { items, error, loading } = useItems();
 
   return (
     <div>
       <Navigation></Navigation>
       {name === 'shopping' ? (
-        <Shopping></Shopping>
+        <Shopping items={items} onClick={addItem} error={error} loading={loading} ></Shopping>
       ) : name === 'cart' ? (
         <Cart></Cart>
       ) : (
--- src/components/Shopping.jsx
+++ src/components/Shopping.jsx
@@ -1,6 +1,18 @@
-function Shopping() {
-    return <div>Shopping</div>;
-  }
+import Card from './Card';
+
+function Shopping({ items, onClick, error, loading }) {
+
+  if (error) return <div>A network error was encountered</div>;
+  if (loading) return <div>Loading...</div>;
+
+  return(
+    <div>
+      {items.map((item) => (
+        <Card item={item} onClick={onClick} key={item.id} ></Card>
+      ))}
+    </div>
+  );
+}
   
 export default Shopping;
   
