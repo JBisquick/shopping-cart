5f90a5fbc93d12296aaebcf589a4590bc75290bc
JBisquick <felipebirch@gmail.com>
2024-03-25 17:22:10 -0700

    Add number to show how many items you have in the cart

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,16 +1,28 @@
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-function Navigation() {
+function Navigation({ items }) {
   return (
     <div>
       <h1>Odin Shopping</h1>
       <div>
         <Link to="/">Home</Link>
         <Link to="/shopping">Shopping</Link>
-        <Link to="/cart">Cart</Link>
+        <div>
+          <Link to="/cart">Cart</Link>
+          <div>{items}</div>
+        </div>
       </div>
     </div>
   );
+}
+
+Navigation.propTypes = {
+  items: PropTypes.number,
+}
+
+Navigation.defaultProps = {
+  items: 0,
 }
 
 export default Navigation;
